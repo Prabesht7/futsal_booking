985a4511ee29f2322cf8be3e0873e42bb53fc774
Prabesh Thapa <numb1prabesht7@gmail.com>
2024-01-31 16:08:20 +0545

    Added booking model controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'pages#home'
   devise_for :users
+  resources :bookings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
