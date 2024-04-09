791c3cff181960e019aa653bff9fc8bf7ff49864
subink-7 <khadkasubin78@gmail.com>
2024-04-08 22:14:56 -0700

    add about us page and css and html

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users
   resources :bookings
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Route for "/aboutus"
+  get '/aboutus', to: 'pages#aboutus'
 
   # Defines the root path route ("/")
   # root "articles#index"
